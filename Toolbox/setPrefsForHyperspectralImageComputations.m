99892222f092fa32df6379d1e9d4901f0f05d925
np_cottaris <cottaris@sas.upenn.edu>
2015-04-27 18:52:12 -0400

    Added prefs setting script for HyperspectralImageComputations project
    Signed-off-by: np_cottaris <cottaris@sas.upenn.edu>

---
+++ Toolbox/setPrefsForHyperspectralImageComputations.m
@@ -0,0 +1,29 @@
+function setPrefsForHyperspectralImageComputations
+
+    %originalDataBaseDir = '/Volumes/SDXC_64GB/Matlab/Analysis/HyperSpectralImages/ManchesterData';
+    originalDataBaseDir = '/Volumes/ColorShare1/hyperspectral-images';
+    isetbioDataBaseDir  = '/Users1/Shared/Matlab/Analysis/hyperspectral-images';
+    
+    % Specify project-specific preferences
+    p = struct( ...
+        'projectName', 'HyperSpectralImageComputations', ...
+        'originalDataBaseDir',  originalDataBaseDir,...
+        'isetbioSceneDataBaseDir', isetbioDataBaseDir ...
+        );
+    
+    generatePreferenceGroup(p);
+end
+
+function generatePreferenceGroup(p)
+    % remove any existing preferences for this project
+    if ispref(p.projectName)
+        rmpref(p.projectName);
+    end
+    
+    % generate and save the project-specific preferences
+    preferences = setdiff(fieldnames(p), 'projectName')
+    for k = 1:numel(preferences)
+        setpref(p.projectName, preferences{k}, p.(preferences{k}));
+    end
+    fprintf('Generated and saved preferences specific to the ''%s'' project.\n', p.projectName);
+end
