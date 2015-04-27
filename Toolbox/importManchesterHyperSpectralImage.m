4ecfd65c1d4a3d3c9f9fb0a6bf0b51bbe8afdf6c
np_cottaris <cottaris@sas.upenn.edu>
2015-04-27 18:53:59 -0400

    Script to import the Machester database
    Signed-off-by: np_cottaris <cottaris@sas.upenn.edu>

---
+++ Toolbox/importManchesterHyperSpectralImage.m
@@ -0,0 +1,50 @@
+function importManchesterHyperSpectralImage
+   
+    sceneData = struct(...
+        'database', 'manchester_database', ...                                          % database directory
+        'name', 'scene4', ...                                                           % scene subdirectory
+        'referencePaintFileName',  'ref_n7.mat', ...                                    % name of reference paint data file
+        'reflectanceDataFileName', 'ref_cyflower1bb_reg1.mat', ...                      % name of scene reflectance data file
+        'spectralRadianceDataFileName', 'radiance_by_reflectance_cyflower1.mat' ...     % name of spectral radiance factor to convert scene reflectance to radiances in Watts/steradian/m^2/nm - akin to the scene illuminant
+    );
+    
+    % Instantiate a HyperSpectralImageDataExtractor
+    hyperSpectralImageDataHandler = HyperSpectralImageDataExtractor(sceneData);
+   
+    % Show an sRGB version of the hyperspectral image with the reference object outlined in red
+    clipLuminance = 12000; gammaValue = 1.7;
+    hyperSpectralImageDataHandler.showLabeledsRGBImage(clipLuminance, gammaValue);
+    
+    resp = input('Export isetbio scene object ? [1=yes] : ');
+    if (resp ~= 1)
+        disp('Bye bye');
+        return;
+    end
+
+    % Export isetbio scene object
+    fileNameOfExportedSceneObject = hyperSpectralImageDataHandler.exportIsetbioSceneObject();
+    
+    test(fileNameOfExportedSceneObject)
+end
+
+
+ function test(fileNameOfExportedSceneObject)
+    % Load exported scene object
+    load(fileNameOfExportedSceneObject);
+    % display scene
+    vcAddAndSelectObject(scene); sceneWindow;
+    
+    % human optics
+    oi = oiCreate('human');
+    
+    % Compute optical image of scene and
+    oi = oiCompute(scene,oi);
+    
+    % Shown optical image
+    vcAddAndSelectObject(oi); oiWindow;
+end
+
+
+
+
+
