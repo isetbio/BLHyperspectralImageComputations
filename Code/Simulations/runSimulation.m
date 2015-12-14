d66573c2a91f1ef85fc71ab1298575d456b1bc56
Nicolas Cottaris <cottaris@sas.upenn.edu>
2015-12-14 16:45:04 -0500

    Added more images in simulation

--- Code/Simulations/runSimulation.m
+++ Code/Simulations/runSimulation.m
@@ -6,7 +6,7 @@ function runSimulation
     recomputePhotonAbsorptionMatrices = false;
     
     sceneSet{1}.dataBaseName = 'manchester_database';
-    sceneSet{1}.sceneNames = {'scene1', 'scene2', 'scene3', 'scene4', 'scene6', 'scene7', 'scene8'};
+    sceneSet{1}.sceneNames = {'scene1', 'scene2', 'scene3', 'scene4', 'scene6', 'scene7', 'scene8', 'scene9', 'scene13', 'scene14', 'scene15'};
     
     sceneSet{2}.dataBaseName = 'harvard_database';
     sceneSet{2}.sceneNames = {...
