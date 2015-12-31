cb36925c5944256b8110f600dffc5e4ffe76f17b
Nicolas Cottaris <cottaris@sas.upenn.edu>
2015-12-31 13:56:58 -0500

    Updates

--- Code/Simulations/StimulusDecoding/downloadIsetbioScenes.m
+++ Code/Simulations/StimulusDecoding/downloadIsetbioScenes.m
@@ -7,12 +7,43 @@ function downloadIsetbioScenes
     
     
     % Retrieve Stanford memorial church scene from Stanford database
-    client.crp('resources/scenes/hyperspectral/stanford_database');
-    [artifactData, artifactInfo] = client.readArtifact('StanfordMemorial', 'type', 'mat');
-    artifactData
-    artifactInfo
+    tic
+    %client.crp('resources/scenes/hyperspectral/stanford_database');
+    %[artifactData, artifactInfo] = client.readArtifact('StanfordMemorial', 'type', 'mat')
     
-    % Compute optical image
+    client.crp('resources/scenes/hyperspectral/manchester_database');
+    [artifactData, artifactInfo] = client.readArtifact('scene1', 'type', 'mat')
+    
+    toc
+    
+    % Compute and show scene
+    tic
+    if ismember('scene', fieldnames(artifactData))
+        fprintf('data contains uncompressed scene data');
+    else
+        fprintf('data contains compressed scene data');
+        basis = artifactData.basis;
+        comment = artifactData.comment;
+        illuminant = artifactData.illuminant;
+        mcCOEF = artifactData.mcCOEF;
+        save('tmp.mat', 'basis', 'comment', 'illuminant', 'mcCOEF');
+        wList = 380:5:780;
+        scene = sceneFromFile('tmp.mat', 'multispectral', [],[],wList);
+        scene = sceneSet(scene, 'distance', artifactData.dist);
+        scene = sceneSet(scene, 'wangular', artifactData.fov);
+        delete('tmp.mat');
+    end
+    toc
+    vcAddAndSelectObject(scene); sceneWindow;
+    
+    % Compute and show optical image
+    tic
+    oi = oiCreate('human');
+    oi = oiCompute(oi, scene);
+    toc
+    vcAddAndSelectObject(oi); oiWindow;
+    
+    % Save optical image
     getpref('HyperSpectralImageIsetbioComputations','opticalImagesCacheDir')
     
 end
