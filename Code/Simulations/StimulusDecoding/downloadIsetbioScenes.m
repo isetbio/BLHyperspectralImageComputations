5021f5d889d6bc73cf8d651aaf16acec107beb99
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-01-01 10:30:22 -0500

    Updates

--- Code/Simulations/StimulusDecoding/downloadIsetbioScenes.m
+++ Code/Simulations/StimulusDecoding/downloadIsetbioScenes.m
@@ -5,52 +5,55 @@ function downloadIsetbioScenes
     % Set up remote data toolbox client
     client = RdtClient(getpref('HyperSpectralImageIsetbioComputations','remoteDataToolboxConfig'));
     
+    % Spacify images
+    imageSources = {...
+        {'stanford_database', 'StanfordMemorial'}, ...
+        {'manchester_database', 'scene1'} ...
+        };
     
-    % Retrieve Stanford memorial church scene from Stanford database
-    tic
-    %client.crp('resources/scenes/hyperspectral/stanford_database');
-    %[artifactData, artifactInfo] = client.readArtifact('StanfordMemorial', 'type', 'mat')
+    % Get directory location where optical images are to be saved
+    getpref('HyperSpectralImageIsetbioComputations','opticalImagesCacheDir');
     
-    % Retrieve forest scene from Manchester database
-    client.crp('resources/scenes/hyperspectral/manchester_database');
-    [artifactData, artifactInfo] = client.readArtifact('scene1', 'type', 'mat')
-    
-    toc
-    
-    % Compute fulland show scene
-    tic
-    if ismember('scene', fieldnames(artifactData))
-        fprintf('Fethed scene contains uncompressed scene data.\n');
-        scene = artifactData.scene;
-    else
-        fprintf('Fetched scene contains compressed scene data.\n');
-        basis = artifactData.basis;
-        comment = artifactData.comment;
-        illuminant = artifactData.illuminant;
-        mcCOEF = artifactData.mcCOEF;
-        save('tmp.mat', 'basis', 'comment', 'illuminant', 'mcCOEF');
-        wList = 380:5:780;
-        scene = sceneFromFile('tmp.mat', 'multispectral', [],[],wList);
-        scene = sceneSet(scene, 'distance', artifactData.dist);
-        scene = sceneSet(scene, 'wangular', artifactData.fov);
-        delete('tmp.mat');
+    for imageIndex = 1:numel(imageSources)
+        % retrieve scene
+        imsource = imageSources{imageIndex};
+        client.crp(sprintf('/resources/scenes/hyperspectral/%s', imsource{1}));
+        [artifactData, artifactInfo] = client.readArtifact(imsource{2}, 'type', 'mat');
+        if ismember('scene', fieldnames(artifactData))
+            fprintf('Fethed scene contains uncompressed scene data.\n');
+            scene = artifactData.scene;
+        else
+            fprintf('Fetched scene contains compressed scene data.\n');
+            scene = uncompressScene(artifactData);
+        end
+        
+        % Show scene
+        vcAddAndSelectObject(scene); sceneWindow;
+        
+        % Compute optical image
+        oi = oiCreate('human');
+        oi = oiCompute(oi, scene);
+        
+        % Show optical image
+        vcAddAndSelectObject(oi); oiWindow;
     end
-    toc
     
-    vcAddAndSelectObject(scene); sceneWindow;
     
-    % Compute and show optical image
-    tic
-    oi = oiCreate('human');
-    oi = oiCompute(oi, scene);
-    toc
-    vcAddAndSelectObject(oi); oiWindow;
-    
-    % Save optical image
-    getpref('HyperSpectralImageIsetbioComputations','opticalImagesCacheDir')
     
 end
 
+function scene = uncompressScene(artifactData)
+    basis      = artifactData.basis;
+    comment    = artifactData.comment;
+    illuminant = artifactData.illuminant;
+    mcCOEF     = artifactData.mcCOEF;
+    save('tmp.mat', 'basis', 'comment', 'illuminant', 'mcCOEF');
+    wList = 380:5:780;
+    scene = sceneFromFile('tmp.mat', 'multispectral', [],[],wList);
+    scene = sceneSet(scene, 'distance', artifactData.dist);
+    scene = sceneSet(scene, 'wangular', artifactData.fov);
+    delete('tmp.mat');
+    end
 function addNeddedToolboxesToPath()
     [rootPath,~] = fileparts(which(mfilename));
     cd(rootPath);
