a4b318623cda96ff7b43e31a4e766a107dbfb598
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-02-19 18:26:43 -0500

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- Code/Simulations/StimulusDecoding/computeDecodingFilter.m
+++ Code/Simulations/StimulusDecoding/computeDecodingFilter.m
@@ -5,7 +5,6 @@ function computeDecodingFilter
         'trainingTimeAxis', 'trainingPhotocurrents', 'trainingLcontrastSequence', 'trainingMcontrastSequence', 'trainingScontrastSequence');
     
     minTimeBin = min([0 min([designMatrix.lat 0])])
-    pause
     rowsOfX = designMatrix.T + minTimeBin;
     
     X = zeros(rowsOfX, 1+(designMatrix.n*designMatrix.m), 'single');
@@ -17,7 +16,7 @@ function computeDecodingFilter
     X(:,1) = 1;
     for row = 1:rowsOfX
         timeBins = designMatrix.lat + row + (0:(designMatrix.m-1)) - minTimeBin;
-        [row rowsOfX size(X,2) timeBins(1) timeBins(end) size(trainingPhotocurrents,2)]
+      %  [row rowsOfX size(X,2) timeBins(1) timeBins(end) size(trainingPhotocurrents,2)]
         for coneIndex = 1:designMatrix.n
             startingColumn = 2 + (coneIndex-1)*designMatrix.m;
             endingColumn = startingColumn + designMatrix.m - 1;
@@ -41,5 +40,14 @@ function computeDecodingFilter
     sizeXbefore
     sizeXafter = size(X)
 
+    tic
+    pseudoInverseOfX = pinv(X);
+    fprintf('Pseudo inverse computation took %2.2f seconds\n', toc);
+    
+    wLcone = pseudoInverseOfX  * lConeC;
+    wMcone = pseudoInverseOfX  * mConeC;
+    wScone = pseudoInverseOfX  * sConeC;
+    
+    save('DecodingFilters.mat', 'wLcone', 'wMcone', 'wScone'); 
 end
 
