c47e479a4e1062888bed2faec715751f4cbf2246
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-03-02 16:09:18 -0500

    Added getScansDir
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

---
+++ Code/Simulations/StimulusDecoding/getScansDir.m
@@ -0,0 +1,18 @@
+function scansDir = getScansDir(rootPath, configuration, adaptingFieldType, osType)
+
+    if (strcmp(osType, 'biophysics-based'))
+        osDir = 'BiophysOSresponses';
+    else
+        osDir = 'LinearOSresponses';
+    end
+    
+    scansDir = sprintf('%s/ScansData.%sConfig/%s/%s', rootPath, configuration, adaptingFieldType, osDir);
+    if (exist(scansDir, 'dir') == false)
+        fprintf('%s folder does not exist. Will create it\n', scansDir);
+        mkdir(scansDir);
+    else
+        fprintf('\nFolder exists at %s.', scansDir);
+    end
+
+end
+        
