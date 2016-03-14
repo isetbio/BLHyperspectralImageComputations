0ef5809cd36fcf17d308360d1db99a1b8caaf83a
Nicolas Cottaris <nicolas@wal-isc-video015.isc-video.upenn.edu>
2016-03-14 16:13:42 -0400

    Updates to prefs

--- Code/setPrefsForHyperspectralImageIsetbioComputations.m
+++ Code/setPrefsForHyperspectralImageIsetbioComputations.m
@@ -1,17 +1,28 @@
 function setPrefsForHyperspectralImageIsetbioComputations
 
-    % Settings for Manta
-    originalDataBaseDir = '/Volumes/Manta TM HD/Dropbox (Aguirre-Brainard Lab)/IBIO_data/BLHyperspectralImageComputations/HyperSpectralImages';
-    isetbioSceneDataBaseDir = 'Volumes/Manta TM HD/Dropbox (Aguirre-Brainard Lab)/IBIO_analysis/BLHyperspectralImageComputations/isetbioScenes';
-    opticalImagesCacheDir  = '/Volumes/Manta TM HD/Dropbox (Aguirre-Brainard Lab)/IBIO_analysis/BLHyperspectralImageComputations/isetbioOpticalImages';
-   
+    sysInfo = GetComputerInfo();
+    
+    % Select dropbox location based on computer name
+    switch (sysInfo.localHostName)
+        case 'IoneanPelagos'
+            dropBoxLocation = '/Volumes/IoneanPelagosTM/Dropbox/Dropbox (Aguirre-Brainard Lab)';
+        case 'Manta'
+            % dropbox location for Manta (Nicolas' iMac)
+            dropBoxLocation = '/Volumes/Manta TM HD/Dropbox (Aguirre-Brainard Lab)';
+    end
+    
+    originalDataBaseDir     = sprintf('%s/IBIO_data/BLHyperspectralImageComputations/HyperSpectralImages', dropBoxLocation);
+    isetbioSceneDataBaseDir = sprintf('%s/IBIO_analysis/BLHyperspectralImageComputations/isetbioScenes', dropBoxLocation);
+    opticalImagesCacheDir   = sprintf('%s/IBIO_analysis/BLHyperspectralImageComputations/isetbioOpticalImages', dropBoxLocation);
+
+    
     % Specify project-specific preferences
     p = struct( ...
-        'projectName', 'HyperSpectralImageIsetbioComputations', ...
+        'projectName',             'HyperSpectralImageIsetbioComputations', ...
         'isetbioSceneDataBaseDir',  isetbioSceneDataBaseDir, ... % where to put the scene files (before they are uploaded to archiva)
-        'originalDataBaseDir',  originalDataBaseDir,...   % where the original data live
+        'originalDataBaseDir',      originalDataBaseDir,...   % where the original data live
         'remoteDataToolboxConfig', '/Users/nicolas/Documents/1.code/2.matlabDevs/ProjectPrefs/rdt-config-isetbio-nicolas.json', ...
-        'opticalImagesCacheDir', opticalImagesCacheDir ...
+        'opticalImagesCacheDir',    opticalImagesCacheDir ...
         );
     
     generatePreferenceGroup(p);
