8ee0a808e4321149e5b779ef26cad7179dd86b50
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-03-17 18:40:04 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

---
+++ SceneReconstruction/+core/sceneAddAdaptingField.m
@@ -0,0 +1,19 @@
+function sceneWithAdaptingField = sceneAddAdaptingField(scene, adaptingFieldParams, borderCols)
+
+    adaptingFieldScene = core.generateAdaptingFieldScene(scene, adaptingFieldParams);
+    
+    sceneWithAdaptingField = scene;
+    
+    % Brute forcing the photons in the perimiter to that of the adapting field
+    borderRows = 1:size(scene.data.photons,1);
+    % override left side
+    sceneWithAdaptingField.data.photons(borderRows, 1:borderCols,:) = ...
+        adaptingFieldScene.data.photons(borderRows, 1:borderCols,:);
+    
+    % override right side
+%     sceneWithAdaptingField.data.photons(borderRows, size(scene.data.photons,2)+(0:-1:-borderCols+1),:) = ...
+%         adaptingFieldScene.data.photons(borderRows, size(scene.data.photons,2)+(0:-1:-borderCols+1),:);
+%     
+        
+end
+
