de9d37c943d543929bd29bc20d5d9e7d6c361188
Nicolas Cottaris <nicolas@wal-isc-video015.isc-video.upenn.edu>
2016-03-22 11:10:46 -0400

    Updates

--- SceneReconstruction/+decoder/computeDecodingFilter.m
+++ SceneReconstruction/+decoder/computeDecodingFilter.m
@@ -3,42 +3,43 @@ function computeDecodingFilter(sceneSetName, descriptionString)
     decodingDataDir = core.getDecodingDataDir(descriptionString);
     fileName = fullfile(decodingDataDir, sprintf('%s_trainingDesignMatrices.mat', sceneSetName));
     
-    fprintf('\nLoading design matrix and stimulus vector ... ');
-    load(fileName, 'Xtrain', 'Ctrain', 'originalTrainingStimulusSize');
-    fprintf('Done\n');
+    tic
+    fprintf('\n1. Loading design matrix and stimulus vector ... ');
+    load(fileName, 'Xtrain', 'Ctrain', 'originalTrainingStimulusSize', 'expParams');
+    fprintf('Done after %2.1f minutes.\n', toc/60);
     
-    fprintf('\nComputing optimal linear decoding filter: pinv(X) ... ');
+    fprintf('\n2. Computing optimal linear decoding filter: pinv(X) [%d x %d] ... ', size(Xtrain,1), size(Xtrain,2));
     pseudoInverseOfX = pinv(Xtrain);
-    fprintf('Done\n');
+     fprintf('Done after %2.1f minutes.\n', toc/60);
     
-    fprintf('\nComputing optimal linear decoding filter: coefficients ... ');
     featuresNum = size(Xtrain,2);
-    stimulusDimensions = size(cTrain,2);
+    stimulusDimensions = size(Ctrain,2);
+    fprintf('\n3. Computing optimal linear decoding filter: coefficients [%d x %d] ... ', featuresNum, stimulusDimensions);
     wVector = zeros(featuresNum, stimulusDimensions);
     for stimDim = 1:stimulusDimensions
-        wVector(:,stimDim) = pseudoInverseOfX * cTrain(:,stimDim);
+        wVector(:,stimDim) = pseudoInverseOfX * Ctrain(:,stimDim);
     end
-    fprintf('Done\n');
+     fprintf('Done after %2.1f minutes.\n', toc/60);
     
-    fprintf('\nComputing in-sample predictions ...');
-    cTrainPrediction = cTrain*0;
+    fprintf('\n4. Computing in-sample predictions [%d x %d]...',  size(Xtrain,1), stimulusDimensions);
+    CtrainPrediction = Ctrain*0;
     for stimDim = 1:stimulusDimensions
-        cTrainPrediction(:, stimDim) = Xtrain * wVector(:,stimDim);
+        CtrainPrediction(:, stimDim) = Xtrain * wVector(:,stimDim);
     end
-    fprintf('Done\n');
+     fprintf('Done after %2.1f minutes.\n', toc/60);
     
-    [trainingSceneLMScontrastSequencePrediction,~] = ...
-        decoder.stimulusSequenceToDecoderFormat(CtrainPrediction, 'fromDecoderFormat', originalTrainingStimulusSize);
-
-    size(testingSceneLMScontrastSequencePrediction)
-    size(testingSceneLMScontrastSequence)
-    
-    fprintf('\nSavind decoder filter and in-sample prediction ... ');
+    fprintf('\n5. Saving decoder filter and in-sample prediction ... ');
     fileName = fullfile(decodingDataDir, sprintf('%s_decodingFilter.mat', sceneSetName));
     save(fileName, 'wVector');
     fileName = fullfile(decodingDataDir, sprintf('%s_inSamplePrediction.mat', sceneSetName));
-    save(fileName, 'cTrain', 'CtrainPrediction');
-    fprintf('Done\n');
+    save(fileName,  'CtrainPrediction', 'originalTrainingStimulusSize', 'expParams');
+    fprintf('Done after %2.1f minutes.\n', toc/60);
+    
+    %  [trainingSceneLMScontrastSequencePrediction,~] = ...
+    %    decoder.stimulusSequenceToDecoderFormat(CtrainPrediction, 'fromDecoderFormat', originalTrainingStimulusSize);
+
+    %size(testingSceneLMScontrastSequencePrediction)
+    %size(testingSceneLMScontrastSequence)
     
     %     'Xtest', 'Ctest', 'originalTestingStimulusSize', ...
     %     'expParams');
--- SceneReconstruction/RunExperiment.m
+++ SceneReconstruction/RunExperiment.m
@@ -68,7 +68,7 @@ function RunExperiment
     % What to compute
     instructionSet = {'compute outer segment responses'};
     instructionSet = {'assembleTrainingDataSet', 'manchester', 'AdaptEvery5Fixations'};
-    instructionSet = {'computeDecodingFilter',  'manchester', 'AdaptEvery5Fixations'}
+    instructionSet = {'computeDecodingFilter',  'manchester', 'AdaptEvery5Fixations'};
 
     switch instructionSet{1}
         case 'compute outer segment responses'
