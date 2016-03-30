b81a1a453b21b5eb5b1de87c8b92ff3a6492e293
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-03-29 22:06:41 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- SceneReconstruction/+decoder/checkDesignMatrixRank.m
+++ SceneReconstruction/+decoder/checkDesignMatrixRank.m
@@ -8,9 +8,19 @@ function checkDesignMatrixRank(sceneSetName, descriptionString)
     load(fileName, 'Xtrain');
     fprintf('Done after %2.1f minutes.\n', toc/60);
     
-    fprintf('Computing matrix rank');
-    fprintf('Rank of design matrix, which has %d features, is : %d \n', size(Xtrain,2), rank(Xtrain));
+    fprintf('\n2. Computing design matrix rank');
+    fprintf('\n3. Rank of design matrix, which has %d features, is : %d.', size(Xtrain,2), rank(Xtrain));
     
+    fprintf('\nComputing singular values of design matrix ...');
+    [U, S, V] = svd(Xtrain);
+    singularValues = diag(S);
+    singularValues = singularValues/singularValues(1);
+    numel(singularValues(singularValues > 1e-10))
     
+    figure(1);
+    clf;
+    plot(1:numel(singularValues), singularValues, 'k.');
+    xlabel('singular value index')
+    ylabel('singular value');
     
 end
