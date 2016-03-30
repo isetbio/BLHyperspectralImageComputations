7e58eb5dc961108319016f2e90a0ac41e29016ad
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-03-29 22:01:48 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- SceneReconstruction/+decoder/checkDesignMatrixRank.m
+++ SceneReconstruction/+decoder/checkDesignMatrixRank.m
@@ -4,10 +4,13 @@ function checkDesignMatrixRank(sceneSetName, descriptionString)
     fileName = fullfile(decodingDataDir, sprintf('%s_trainingDesignMatrices.mat', sceneSetName));
     
     tic
-    fprintf('\n1. Loading design matrix and stimulus vector ... ');
+    fprintf('\n1. Loading design matrix... ');
     load(fileName, 'Xtrain');
     fprintf('Done after %2.1f minutes.\n', toc/60);
     
+    fprintf('Computing matrix rank');
     fprintf('Rank of design matrix, which has %d features, is : %d \n', size(Xtrain,2), rank(Xtrain));
+    
+    
     
 end
