f5b3d9e75d56c22c0cd77731baa5857e1ca95345
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-04-03 11:17:02 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- SceneReconstruction/+core/LMSRGBconversionData.m
+++ SceneReconstruction/+core/LMSRGBconversionData.m
@@ -6,16 +6,9 @@ function [coneFundamentals, displaySPDs, RGBtoXYZ, wave] = LMSRGBconversionData(
     coneFundamentals = (sensorLMS.T)';
     coneFundamentals = coneFundamentals(validWaveIndices,:);
     
-    
     d = displayCreate(displayName, 'wave', wave);
-    beforeLum = displayGet(d, 'peak luminance')
-    
-    % The higher the gain, the more LMS contrast we can render without going out of gamut
     displaySPDs = displayGet(d, 'spd');
     displaySPDs = displaySPDs*gain;
     d = displaySet(d, 'spd', displaySPDs);
-    afterLum = displayGet(d, 'peak luminance')
-    
     RGBtoXYZ = displayGet(d, 'rgb2xyz');
-    
 end
--- SceneReconstruction/external/cbrewer/generateAndSaveColormaps.m
+++
@@ -1,8 +0,0 @@
-function generateAndSaveColormaps
-
-    spectralLUT = cbrewer('div', 'Spectral', 1024);
-    spectralLUT = spectralLUT(size(spectralLUT,1):-1:1,:);
-    save('CustomColormaps.mat', 'spectralLUT');
-
-end
-
