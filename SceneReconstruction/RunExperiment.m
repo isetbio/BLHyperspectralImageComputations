d74303319fd37e4e966fe6ebe030a9cce99907a5
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-04-08 10:05:55 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- SceneReconstruction/RunExperiment.m
+++ SceneReconstruction/RunExperiment.m
@@ -5,10 +5,10 @@ function RunExperiment
     % Computation steps. Uncomment the ones you want to execute
     instructionSet = {...
        %'lookAtScenes' ...
-       % 'compute outer segment responses' ...      % compute OS responses. Data saved in the scansData directory
-       % 'assembleTrainingDataSet' ...               % generates the training/testing design matrices. Data are saved in the decodingData directory
+        'compute outer segment responses' ...      % compute OS responses. Data saved in the scansData directory
+        'assembleTrainingDataSet' ...               % generates the training/testing design matrices. Data are saved in the decodingData directory
         'computeDecodingFilter' ...                 % computes the decoding filter based on the training data set (in-sample). Data stored in the decodingData directory
-       % 'computeOutOfSamplePrediction' ...          % computes reconstructions based on the test data set (out-of-sample). Data stored in the decodingData directory
+        'computeOutOfSamplePrediction' ...          % computes reconstructions based on the test data set (out-of-sample). Data stored in the decodingData directory
        % 'visualizeScan' ...                        % visualize the responses from one scan - under construction
        % 'visualizeDecodingFilter' ...              % visualize the decoder filter's spatiotemporal dynamics
        % 'visualizeInSamplePrediction' ...          % visualize the decoder's in-sample deperformance
@@ -17,15 +17,15 @@ function RunExperiment
        % 'visualizeConeMosaic' ...                  % visualize the LMS cone mosaic used
         };
   
-    sceneSetName = 'manchester';
-    resultsDir = 'AdaptEvery40Fixations/@osLinear';
-    trainingDataPercentange = 75;
-    testingDataPercentage = 25;
-                
 %    sceneSetName = 'manchester';
-%    resultsDir = 'NoMicroMovements50msPresentationTime/@osLinear';
+%    resultsDir = 'AdaptEvery40Fixations/@osLinear';
 %    trainingDataPercentange = 75;
 %    testingDataPercentage = 25;
+                
+    sceneSetName = 'manchester';
+    resultsDir = 'NoMicroMovements50msPresentationTime/@osLinear';
+    trainingDataPercentange = 75;
+    testingDataPercentage = 25;
     
 %     sceneSetName = 'harvard_manchester';
 %     resultsDir = 'AdaptEvery500Fixations/@osLinear';
