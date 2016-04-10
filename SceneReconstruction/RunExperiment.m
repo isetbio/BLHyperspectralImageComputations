43557a17396c5bb0844691ca5e0d861f8ff2dc85
Nicolas Cottaris <nicolas@wal-isc-video015.isc-video.upenn.edu>
2016-04-10 15:43:51 -0400

    updates

--- SceneReconstruction/RunExperiment.m
+++ SceneReconstruction/RunExperiment.m
@@ -7,10 +7,11 @@ function RunExperiment
        %'lookAtScenes' ...
         'compute outer segment responses' ...      % compute OS responses. Data saved in the scansData directory
         'assembleTrainingDataSet' ...               % generates the training/testing design matrices. Data are saved in the decodingData directory
-        'computeDecodingFilter' ...                 % computes the decoding filter based on the training data set (in-sample). Data stored in the decodingData directory
+       'computeDecodingFilter' ...                 % computes the decoding filter based on the training data set (in-sample). Data stored in the decodingData directory
         'computeOutOfSamplePrediction' ...          % computes reconstructions based on the test data set (out-of-sample). Data stored in the decodingData directory
        % 'visualizeScan' ...                        % visualize the responses from one scan - under construction
-       % 'visualizeDecodingFilter' ...              % visualize the decoder filter's spatiotemporal dynamics
+        %
+        'visualizeDecodingFilter' ...              % visualize the decoder filter's spatiotemporal dynamics
        % 'visualizeInSamplePrediction' ...          % visualize the decoder's in-sample deperformance
        % 'visualizeOutOfSamplePrediction' ...       % visualize the decoder's out-of-sample deperformance
        % 'makeReconstructionVideo' ...              % generate video of the reconstruction
@@ -24,7 +25,7 @@ function RunExperiment
     
 
     sceneSetName = 'harvard_manchester'
-    resultsDir = sprintf('%s/@osLinear', sceneSetName);
+    resultsDir = sprintf('%s/@osLinear', 'Fixation200msMicrofixationGain1');
     trainingDataPercentange = 50;
     testingDataPercentage = 50;
             
@@ -191,7 +192,7 @@ function expParams = experimentParams()
             'meanFixationDurationInMillisecondsForAdaptingField', 200, ...
             'stDevFixationDurationInMillisecondsForAdaptingField', 20, ...
             'microFixationGain', 1, ...
-            'fixationOverlapFactor', 0.3^2, ...     
+            'fixationOverlapFactor', 0.4^2, ...     
             'saccadicScanMode',  'randomized'... %                        % 'randomized' or 'sequential', to visit eye position grid sequentially
         ) ...
     );
