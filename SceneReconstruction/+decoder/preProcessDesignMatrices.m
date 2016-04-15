6959520dfb4eca14d27e0d3208207f0571ea8f6f
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-04-15 07:18:45 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- SceneReconstruction/+decoder/preProcessDesignMatrices.m
+++ SceneReconstruction/+decoder/preProcessDesignMatrices.m
@@ -3,96 +3,97 @@ function preProcessDesignMatrices(sceneSetName, decodingDataDir)
     fileNameXtrain = fullfile(decodingDataDir, sprintf('%s_trainingDesignMatrices.mat', sceneSetName));
     fileNameXtest  = fullfile(decodingDataDir, sprintf('%s_testingDesignMatrices.mat', sceneSetName));
     
-    tic
-    % Load train design matrix
-    fprintf('\n1a. Loading design matrix from ''%s''  ... ', fileNameXtrain);
-    load(fileNameXtrain, 'Xtrain', 'preProcessingParams');
     
+    % Load train design matrix
+    fprintf('\n1. Loading training design matrix from ''%s''  ... ', fileNameXtrain);
+    tic
+    load(fileNameXtrain, 'Xtrain', 'preProcessingParams');
     fprintf('Done after %2.1f minutes.\n', toc/60);
     trainingSamples = size(Xtrain,1);
     filterDimensions = size(Xtrain,2);
     
-    % Load test design matrix
+    fprintf('2. Proprocessing training matrix  [%d x %d]... ', trainingSamples, filterDimensions);
     tic
-    fprintf('1b. Loading test design matrix ''%s''... ', fileNameXtest);
+    Xtrain = preProcessDesignMatrix(Xtrain, preProcessingParams);
+    fprintf('Done after %2.1f minutes.\n', toc/60);
+    
+    fprintf('3. Saving training matrix  ... ');
+    tic
+    save(fileNameXtrain, 'Xtrain', '-append');
+    clear 'Xtrain';
+    
+    
+    % Load test design matrix
+    fprintf('\n1. Loading test design matrix ''%s''... ', fileNameXtest);
+    tic
     load(fileNameXtest, 'Xtest');
     fprintf('Done after %2.1f minutes.\n', toc/60);
     testingSamples = size(Xtest,1);
     
+    fprintf('2. Proprocessing test matrix  [%d x %d]... ', testingSamples, filterDimensions);
     tic
-    fprintf('1c. Computing rank(X) (before pre-processing) [%d x %d]...',  trainingSamples, filterDimensions);
-    XtrainRank = rank(Xtrain);
-    fprintf('Done after %2.1f minutes.\n', toc/60);
-    fprintf('<strong>Rank (X) (before pre-processing) = %d</strong>\n', XtrainRank);
-    
-    % Concatenate matrices into a grand design matrix
-    tic
-    fprintf('1d. Concatenating traing and test design matrices...');
-    Xgrand = cat(1, Xtrain, Xtest);
+    Xtest = preProcessDesignMatrix(Xtest, preProcessingParams);
     fprintf('Done after %2.1f minutes.\n', toc/60);
     
-    clear 'Xtrain'
+    fprintf('3. Saving test matrix  ... ');
+    tic
+    save(fileNameXtest, 'Xtest', '-append');
+    fprintf('Done after %2.1f minutes.\n', toc/60);
     clear 'Xtest'
-    
-    timeSamples = size(Xgrand,1);
-    filterDimensions = size(Xgrand,2);
+end
+
+
+function X = preProcessDesignMatrix(X, preProcessingParams)
+
+    timeSamples = size(X,1);
+    filterDimensions = size(X,2);
     
     if (preProcessingParams.designMatrixBased > 0)
         tic
-        fprintf('2aa. Centering (X) [%d x %d]...',  timeSamples, filterDimensions);
+        fprintf('2a. Centering (X) [%d x %d]...',  timeSamples, filterDimensions);
         
-        % Compute degree of whiteness of Xgrand
-        varianceCovarianceMatrix = 1/timeSamples*(Xgrand')*Xgrand;
+        % Compute degree of whiteness of X
+        varianceCovarianceMatrix = 1/timeSamples*(X')*X;
         upperDiagElements = triu(varianceCovarianceMatrix, 1);
-        originalXgrandCovariances = upperDiagElements(:);
-        normOfOriginalXgrandCovariances = sqrt(1/numel(originalXgrandCovariances)*sum(originalXgrandCovariances.^2));
+        originalXCovariances = upperDiagElements(:);
+        normOfOriginalXCovariances = sqrt(1/numel(originalXCovariances)*sum(originalXCovariances.^2));
    
         % Compute centering operator
         oneColVector = ones(timeSamples,1);
-        designMatrixPreprocessing.centeringOperator = (1/timeSamples*(Xgrand')*oneColVector)';
+        designMatrixPreprocessing.centeringOperator = (1/timeSamples*(X')*oneColVector)';
         
-        % Center Xgrand
-        Xgrand = bsxfun(@minus, Xgrand, designMatrixPreprocessing.centeringOperator);
-        Xgrand(:,1) = 1;
+        % Center X
+        X = bsxfun(@minus, X, designMatrixPreprocessing.centeringOperator);
+        X(:,1) = 1;
         fprintf('Done after %2.1f minutes.\n', toc/60);
         
         if (preProcessingParams.designMatrixBased > 1)  
             tic
-            fprintf('2ab. Normalizing (X) [%d x %d]...',  timeSamples, filterDimensions);
+            fprintf('2b. Normalizing (X) [%d x %d]...',  timeSamples, filterDimensions);
         
             % Compute normalizing operator: divide by stddev
-            designMatrixPreprocessing.normalizingOperator = (1./(sqrt(1/timeSamples*((Xgrand.^2)')*oneColVector)))';
+            designMatrixPreprocessing.normalizingOperator = (1./(sqrt(1/timeSamples*((X.^2)')*oneColVector)))';
 
-            % Normalize Xgrand
-            Xgrand= bsxfun(@times, Xgrand, designMatrixPreprocessing.normalizingOperator);
-            Xgrand(:,1) = 1;
+            % Normalize X
+            X= bsxfun(@times, X, designMatrixPreprocessing.normalizingOperator);
+            X(:,1) = 1;
             fprintf('Done after %2.1f minutes.\n', toc/60);
         
             if (preProcessingParams.designMatrixBased > 2)
                 tic
-                fprintf('2ac. Whitening (X) [%d x %d]...',  timeSamples, filterDimensions);
+                fprintf('2c. Whitening (X) [%d x %d]...',  timeSamples, filterDimensions);
         
                 % Compute whitening operator:
-                Sigma = 1/timeSamples * (Xgrand') * Xgrand;
+                Sigma = 1/timeSamples * (X') * X;
                 [U, Gamma, V] = svd(Sigma, 'econ');
                 designMatrixPreprocessing.whiteningOperator = U * (inv(sqrt(Gamma))) * V';
 
-                % Whiten Xgrand
-                Xgrand = Xgrand * designMatrixPreprocessing.whiteningOperator;
-                Xgrand(:,1) = 1;
+                % Whiten X
+                X = X * designMatrixPreprocessing.whiteningOperator;
+                X(:,1) = 1;
                 fprintf('Done after %2.1f minutes.\n', toc/60);
             end
         end
     end
-    
-    tic
-    fprintf('3. Saving preprocessed design matrices ...');
-    
-    Xtrain = Xgrand(1:trainingSamples,:);
-    Xtest  = Xgrand(trainingSamples+(1:testingSamples),:);
-    clear 'Xgrand'
-    
-    save(fileNameXtrain, 'Xtrain', '-append');
-    save(fileNameXtest, 'Xtest', '-append');
-    fprintf('Done after %2.1f minutes.\n', toc/60);
 end
+
--- SceneReconstruction/RunExperiment.m
+++ SceneReconstruction/RunExperiment.m
@@ -5,7 +5,7 @@ function RunExperiment
     % Computation steps. Uncomment the ones you want to execute
     computationInstructionSet = {...
        %'lookAtScenes' ...
-       'compute outer segment responses' ...       % compute OS responses. Data saved in the scansData directory
+       %'compute outer segment responses' ...       % compute OS responses. Data saved in the scansData directory
        'assembleTrainingDataSet' ...               % generates the training/testing design matrices. Data are saved in the decodingData directory
        'computeDecodingFilter' ...                 % computes the decoding filter based on the training data set (in-sample). Data stored in the decodingData directory
        'computeOutOfSamplePrediction' ...          % computes reconstructions based on the test data set (out-of-sample). Data stored in the decodingData directory
