a301d8ea2101b61cb0cba4306af01cedf25b7464
Nicolas Cottaris <nicolas@wal-isc-video015.isc-video.upenn.edu>
2016-04-20 14:31:03 -0400

    Updates

--- SceneReconstruction/RunExperiment.m
+++ SceneReconstruction/RunExperiment.m
@@ -5,7 +5,7 @@ function RunExperiment
     % Computation steps. Uncomment the ones you want to execute
     computationInstructionSet = {...
        %'lookAtScenes' ...
-       %'compute outer segment responses' ...       % compute OS responses. Data saved in the scansData directory
+       'compute outer segment responses' ...       % compute OS responses. Data saved in the scansData directory
        'assembleTrainingDataSet' ...               % generates the training/testing design matrices. Data are saved in the decodingData directory
        'computeDecodingFilter' ...                 % computes the decoding filter based on the training data set (in-sample). Data stored in the decodingData directory
        'computeOutOfSamplePrediction' ...          % computes reconstructions based on the test data set (out-of-sample). Data stored in the decodingData directory
@@ -22,7 +22,7 @@ function RunExperiment
   
     % Specify what to compute
     instructionSet = computationInstructionSet;  
-    instructionSet = visualizationInstructionSet;
+    %instructionSet = visualizationInstructionSet;
     
     
     % Set data preprocessing params - This affects the name of the decodingDataDir
@@ -34,10 +34,10 @@ function RunExperiment
     
     computeSVDbasedLowRankFiltersAndPredictions = true;
     SVDbasedLowRankFilterVariancesExplained = [80 85 90 92 94 95 96 97 98 99.5 99.9 99.999];
-    
+    %
     % Specify the data set to use
-    whichDataSet =  'original';
-    
+    whichDataSet =  'large';
+
     switch (whichDataSet)
         case 'very_small'
             sceneSetName = 'manchester';  
@@ -56,7 +56,7 @@ function RunExperiment
             
         case 'large'
             sceneSetName = 'harvard_manchester';  
-            scanSpatialOverlapFactor = 0.50; 
+            scanSpatialOverlapFactor = 0.60; 
             fixationsPerScan = 20;
             
         otherwise
